9c1abc27ea050a2921695d9c4bdff12321245aee
ٌRokn design <fastmarketing084@gmail.com>
2026-03-24 09:02:43 +0200

    Update articles-data.js

--- articles-data.js
+++ articles-data.js
@@ -16,3 +16,11 @@ const allArticles = [
     { id: 3, title: "أسرار المحارة الهندسية", desc: "...", img: "...", cat: "تأسيس" },
     { id: 4, title: "تأسيس الكهرباء", desc: "...", img: "...", cat: "تأسيس" }
 ];
+{
+    id: 5,
+    title: "بديل الرخام والخشب: فخامة الفنادق بميزانية ذكية",
+    description: "كيف تحصل على مظهر الرخام الطبيعي ودفء الخشب بتكلفة اقتصادية ومميزات عملية تتفوق على الخامات الطبيعية.",
+    image: "https://images.unsplash.com/photo-1618221195710-dd6b41faaea6",
+    link: "articles/article5.html",
+    category: "ديكور"
+},
